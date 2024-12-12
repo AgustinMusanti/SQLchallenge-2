33eb55a7b4470c4c08d45d064164f2027d6b26ff
Agustin Musanti <138232526+AgustinMusanti@users.noreply.github.com>
2024-12-11 21:19:30 -0300

    >>

--- challenge&solutions.sql
+++ challenge&solutions.sql
@@ -5,12 +5,18 @@ USE TechMarket;
 -- ¿Cuántos clientes se registraron en el año 2023?
 
 SELECT COUNT(*) AS TotalClientes2023
+  
 FROM customers
+  
 WHERE YEAR(signup_date) = 2023;
+
 
 -- Listar los clientes que no realizaron ninguna compra
 
 SELECT c.customer_id, c.customer_name, c.customer_email
+  
 FROM customers c
+  
 LEFT JOIN orders o ON c.customer_id = o.customer_id
+  
 WHERE o.order_id IS NULL;
